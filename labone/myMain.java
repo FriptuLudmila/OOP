29f0d3ee869b2383357f32bf9572248022bce035
ludmila.friptu <ludmila.friptu@iucosoft.com>
2023-10-03 11:56:25 +0300

    Lab one refactor structure.

---
+++ labone/myMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author liuda
+ */
+public class myMain {
+    public static void main (String[] args){
+        Fruits example = new Fruits();
+        example.smoothie1(example.apple, example.banana);
+        String text = example.smoothie2(example.orange, example.banana);
+        System.out.println(text);
+                
+                
+    }
+    
+    
+    
+}
